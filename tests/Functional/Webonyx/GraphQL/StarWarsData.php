8a78a51235d9b5902e44fd21b40439592d4ebb1f
Jeremiah VALERIE <jeremiah@overblog.com>
2021-03-13 15:21:52 +0100

    Fix travis php-cs check

--- tests/Functional/Webonyx/GraphQL/StarWarsData.php
+++ tests/Functional/Webonyx/GraphQL/StarWarsData.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the DataLoaderPhp package.
+ *
+ * (c) Overblog <http://github.com/overblog/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Overblog\DataLoader\Test\Functional\Webonyx\GraphQL;
 
 use function array_map;
